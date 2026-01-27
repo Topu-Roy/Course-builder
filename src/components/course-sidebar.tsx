6150af4a405c6976669d647b0e0fbae7114b6e66
Topu-Roy <topuroy143333@gmail.com>
2026-01-27 12:16:23 +0600

    feat: Add search input field to course sidebar.

--- src/components/course-sidebar.tsx
+++ src/components/course-sidebar.tsx
@@ -161,7 +161,10 @@ export function CourseSidebarSkeleton() {
 
         <SidebarGroup className="py-0">
           <SidebarGroupContent className="relative">
-            <Skeleton className="h-9 w-full" />
+            <Label htmlFor="search" className="sr-only">
+              Search
+            </Label>
+            <SidebarInput id="search" placeholder="Search the docs..." className="pl-8" />
             <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
           </SidebarGroupContent>
         </SidebarGroup>
